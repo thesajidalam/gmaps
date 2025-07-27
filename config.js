912a32639aac6f3af0fcb3acc20077ed5f43a49a
Mohammad Sajid Alam <117466907+thesajidalam@users.noreply.github.com>
2025-07-27 18:13:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,18 +3,28 @@ const CONFIG = {
         DEFAULT_CENTER: { lat: 37.7749, lng: -122.4194 },
         DEFAULT_ZOOM: 12,
         STYLES: [
-            { featureType: "all", elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-            { featureType: "poi", stylers: [{ visibility: "off" }] }
+            { 
+                featureType: "all", 
+                elementType: "geometry", 
+                stylers: [{ color: "#242f3e" }] 
+            },
+            { 
+                featureType: "poi", 
+                stylers: [{ visibility: "off" }] 
+            }
         ]
     },
     TIME: {
         MIN_YEAR: 1990,
         MAX_YEAR: 2025,
-        DEFAULT_YEAR: 2025
+        DEFAULT_YEAR: 2025,
+        TRANSITION_DURATION: 500
     },
-    PLACES: {
-        TYPES: ["electronics_store", "university"],
-        RADIUS: 50000
+    MARKERS: {
+        COLORS: {
+            startup: '#6e44ff',
+            office: '#00f7ff',
+            unicorn: '#ff2d75'
+        }
     }
 };
-Object.freeze(CONFIG);
